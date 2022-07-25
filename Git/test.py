d219c19caf9e93772e080e4955c48b37d0838c9e
Ujinkwon <dbwlsdl586@gmail.com>
2022-07-25 15:32:54 +0900

    0725

--- Git/test.py
+++ Git/test.py
@@ -1,7 +1,8 @@
-print('x', 'c', end = '\n')
-
-def add(*args):
-    for arg in args:
-        print(arg)
-
-add(2,3,4,5)
+def get_middle_char(text):
+    if len(text) % 2 == 0:
+        center = len(text) // 2
+        return text[center-1:center + 1]
+    else:
+        center = len(text) // 2
+        return text[center]
+print(get_middle_char('power'))
