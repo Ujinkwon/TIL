0aecfffe3de83f3d10908c8c942053b5562e9715
Ujinkwon <dbwlsdl586@gmail.com>
2022-09-08 00:57:12 +0900

    0908

--- Git/Django/0907_사용자 인증 및 권한/accounts/views.py
+++ Git/Django/0907_사용자 인증 및 권한/accounts/views.py
@@ -1,18 +1,77 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 # Create your views here.
 def login(request):
+    # 로그인한 유저는 로그인 창 띄우지 않도록
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
             # 로그인
             auth_login(request, form.get_user())
-            return redirect('articles:index')
+            return redirect(request.GET.get('next') or 'articles:index')
     else:
         form = AuthenticationForm()
     context = {
         'form': form,
     }
     return render(request, 'accounts/login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
+
+def signup(request):
+    if request.method == 'POST':
+        form =  CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+def delete(request):
+    # 회원탈퇴
+    request.user.delete()
+    return redirect('articles:index')
+
+def update(request):
+    # 회원정보 수정
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+def change_password(request):
+    # 비밀번호 변경
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('articles:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/change_password.html', context)
