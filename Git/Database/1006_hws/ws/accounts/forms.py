8b984aa598052e0c379ae325f477c704846fd5ca
Ujinkwon <dbwlsdl586@gmail.com>
2022-10-07 20:21:53 +0900

    1007

--- Git/Database/1006_hws/ws/accounts/forms.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import field
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = get_user_model()
-        fields = UserCreationForm.Meta.fields
